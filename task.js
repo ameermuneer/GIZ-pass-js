4a329f9dae2a46344ebc7c08ac5a66bf03a6666f
Ameer Muneer <96ameermuneer@gmail.com>
2021-10-01 00:08:06 +0300

    refactoring bubble sort algorithm

--- task.js
+++ task.js
@@ -9,36 +9,41 @@ let numbers = [
     16
 ];
 
-console.log("Original numbers list: ", numbers)
+console.log("Original numbers list: ", numbers);
 
-for(let i = 0 ; i < numbers.length ; i++)
-{
-    for(let j = 0 ; j < numbers.length - 1 ; j++)
-    {
-        if(numbers[i] > numbers[j + 1])
-        {
+// flag to be true when there one iteration withou any swapping
+let is_sorting_finished = false;
+while (!is_sorting_finished) {
+    is_sorting_finished = true;
+    for (let j = 0; j < numbers.length - 1; j++) {
+        if (numbers[j] > numbers[j + 1]) {
+            // swapping
             let temp = numbers[j];
             numbers[j] = numbers[j + 1]
             numbers[j + 1] = temp;
+            // when swapping , then another iteration is needed 
+            is_sorting_finished = false;
         }
     }
 }
 
-console.log("Numbers list After sorting: ", numbers)
+console.log("Numbers list After sorting: ", numbers);
 
-for(let i = 0 ; i < numbers.length ; i++)
-{
-    for(let j = 0 ; j < numbers.length - 1 ; j++)
-    {
-        if(numbers[i] < numbers[j + 1])
-        {
+// flag to be true when there one iteration without any swapping
+is_sorting_finished = false;
+while (!is_sorting_finished) {
+    is_sorting_finished = true;
+    for (let j = 0; j < numbers.length - 1; j++) {
+        if (numbers[j] < numbers[j + 1]) {
+            // swapping
             let temp = numbers[j];
-            numbers[j] = numbers[j + 1]
+            numbers[j] = numbers[j + 1];
             numbers[j + 1] = temp;
+            // when swapping , then another iteration is needed 
+            is_sorting_finished = false;
         }
     }
 }
 
-console.log("Numbers list After Desc sorting: ", numbers)
-
+console.log("Numbers list After Desc sorting: ", numbers);
 
